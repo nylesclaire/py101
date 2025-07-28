40b7f6104c8cb66fc10aba6283787c582d9ba3ec
nylesclaire <149444393+nylesclaire@users.noreply.github.com>
2025-07-28 13:03:30 +0000

    Refined input validation for mortgage calculator.

--- lesson_2/mortgage_calc.py
+++ lesson_2/mortgage_calc.py
@@ -7,9 +7,13 @@ def get_valid_principal():
     prompt('What is the loan amount?')
     while True:
         user_principal = input()
+        user_principal = user_principal.replace(',', '')
         try:
             user_principal = float(user_principal)
-            break
+            if user_principal > 0:
+                break
+            prompt("Please provide a positive loan amount.")
+            continue
         except ValueError:
             prompt("That doesn't seem to be a valid number-- try again.")
             continue
@@ -25,39 +29,44 @@ def get_valid_apr():
             user_apr = float(user_apr)
             if 0 <= user_apr < 100:
                 break
-            prompt("That doesn't seem to be a valid number-- try again.")
+            prompt("Please provide a valid percentage.")
             continue
         except ValueError:
             prompt("That doesn't seem to be a valid number-- try again.")
             continue
     return user_apr
 
-
 def get_valid_duration():
     prompt("""What is the duration of the loan? Enter the number of months,
            or press 'y' to enter the duration in years.""")
     while True:
         user_loan_dur = input()
-        if user_loan_dur in ['y','Y','years']:
-            prompt("What is the duration in years?")
-            while True:
-                years_dur = input()
-                try:
-                    years_dur = float(years_dur)
-                    user_loan_dur = years_dur * 12
-                    break
-                except ValueError:
-                    prompt("That doesn't seem to be \
-                           a valid number-- try again.")
-                    continue
+        if user_loan_dur in ['y','Y','years','Years']:
+            user_loan_dur = years_dur_to_months()
         try:
-            user_loan_dur = int(user_loan_dur)
-            break
+            user_loan_dur = float(user_loan_dur)
+            if 0 < user_loan_dur:
+                break
+            prompt("Please provide a positive duration.")
+            continue
         except ValueError:
             prompt("That doesn't seem to be a valid number-- try again.")
             continue
     return user_loan_dur
 
+def years_dur_to_months():
+    prompt("What is the duration in years?")
+    while True:
+        years_dur = input()
+        try:
+            years_dur = float(years_dur)
+            loan_in_months = years_dur * 12
+            break
+        except ValueError:
+            prompt("That doesn't seem to be \
+                    a valid number-- try again.")
+            continue
+    return loan_in_months
 
 # Welcome message
 prompt('Welcome to the Mortgage or Car Loan calculator.')
@@ -87,9 +96,9 @@ while True:
 
     # Ask user if they'd like to go again
     prompt("""Would you like to perform another calculation?
-            Press 'Y' to continue.""")
+            Press 'Y' to continue, or any other key to exit.""")
     again = input()
-    if again in ['y','Y']:
+    if again in ['y','Y','yes']:
         prompt("------------------------------------")
         continue
     prompt("Thanks for using the calculator. Bye!")
