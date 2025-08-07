e0e90d6dfd1c22da48e2e1a19f129ee987092f12
nylesclaire <149444393+nylesclaire@users.noreply.github.com>
2025-08-07 18:57:21 +0000

    Added 'Best of Five' functionality to Rock, Paper, Scissors program.

--- lesson_2/rock_paper_scissors.py
+++ lesson_2/rock_paper_scissors.py
@@ -22,6 +22,14 @@ def prompt(message):
 def player_won(player_choice, computer_choice):
     if computer_choice in WINNER_COMBOS[player_choice]:
         return True
+    else:
+        return False
+    
+def computer_won(player_choice, computer_choice):
+    if player_choice in WINNER_COMBOS[computer_choice]:
+        return True
+    else:
+        return False
 
 def display_winner(player, computer):
     prompt(f"You chose {player}, computer chose {computer}")
@@ -33,21 +41,40 @@ def display_winner(player, computer):
         prompt("The computer won!")
 
 #Begin main program
-while True:
-    prompt(f"Choose one:")
-    for key, value in CHOICES_DICT.items():
-        print(f'{key} for {value}') 
-    short_choice = input().lower()
+player_total_wins = 0
+computer_total_wins = 0
+current_game = 1
 
-    while short_choice not in list(CHOICES_DICT):
-        prompt("That's not a valid choice")
+while True:
+    prompt("Let's play Best of Five!")
+    while player_total_wins < 3 and computer_total_wins < 3:
+        prompt(f"Game {current_game}:")
+        prompt(f"Choose one:")
+        for key, value in CHOICES_DICT.items():
+            print(f'{key} for {value}') 
         short_choice = input().lower()
 
-    choice = CHOICES_DICT[short_choice]
+        while short_choice not in list(CHOICES_DICT):
+            prompt("That's not a valid choice")
+            short_choice = input().lower()
 
-    computer_chooses = random.choice(list(CHOICES_DICT.values()))
+        choice = CHOICES_DICT[short_choice]
 
-    display_winner(choice, computer_chooses)
+        computer_chooses = random.choice(list(CHOICES_DICT.values()))
+
+        display_winner(choice, computer_chooses)
+
+        current_game += 1
+
+        if player_won(choice, computer_chooses):
+            player_total_wins += 1
+        elif computer_won(choice, computer_chooses):
+            computer_total_wins += 1
+
+    if player_total_wins <= 3:
+        prompt("You're the grand winner!")
+    else:
+        prompt("The computer is the grand winner!")
 
     while True:
         prompt("Do you want to play again (y/n)?")
@@ -60,3 +87,5 @@ while True:
 
     if answer[0] == 'n':
         break
+    else:
+        prompt("Thanks for playing. Bye!")
