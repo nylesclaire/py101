b9e6c85db1c73203ddb82444861ec1bde1d13389
Nyles Claire Suer <nylesclaire@gmail.com>
2026-01-07 15:48:36 +0000

    Finished removing files I moved to new folder.

--- small_problems/2_odd_numbers.py
+++
@@ -1,8 +0,0 @@
-# Print all odd numbers from 1 to 99, inclusive, 
-# with each number on a separate line.
-
-# Bonus Question: Can you solve the problem by 
-# iterating over just the odd numbers?
-
-for element in range(1, 100, 2):
-    print(element)
--- small_problems/3_even_numbers.py
+++
@@ -1,8 +0,0 @@
-# Print all even numbers from 1 to 99, inclusive, 
-# with each number on a separate line.
-
-# Bonus Question: Can you solve the problem by 
-# iterating over just the even numbers?
-
-for element in range(2, 100, 2):
-    print(element)
